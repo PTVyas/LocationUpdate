c95e02995e923de40a49b4539d34f9c7400b80b4
PTVyas <vyasbhagat@gmail.com>
2018-04-26 14:34:35 +0530

    Initial Commit

---
+++ LocationUpdate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationUpdate
+//
+//  Created by WOS_MacMini_1 on 26/04/18.
+//  Copyright © 2018 White Orange Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocationUpdate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LocationUpdate
+//
+//  Created by WOS_MacMini_1 on 26/04/18.
+//  Copyright © 2018 White Orange Software. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
